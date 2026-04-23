b53786653dc7c4e0dad4204e78f3de4a56ab333e
Kristopher Rangel-Olivos <kristopherrangel@ucsb.edu>
2026-04-22 18:30:03 -0700

    kris-helpRequest create database

--- src/main/java/edu/ucsb/cs156/example/repositories/HelpRequestRepository.java
+++ src/main/java/edu/ucsb/cs156/example/repositories/HelpRequestRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
-/** The HelpRequestRepository is a repository for UCSBDate entities. */
+/** The HelpRequestRepositoryy is a repository for UCSBDate entities. */
 @Repository
 @RepositoryRestResource(exported = false)
 public interface HelpRequestRepository extends CrudRepository<HelpRequest, Long> {}

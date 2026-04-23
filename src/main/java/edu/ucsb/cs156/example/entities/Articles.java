5b55e7a3205b2e2904a6022d8789c07339824271
Xuanbo Zhao <xuanbo@ucsb.edu>
2026-04-22 22:57:53 -0700

    create database table for Articles

--- src/main/java/edu/ucsb/cs156/example/entities/Articles.java
+++ src/main/java/edu/ucsb/cs156/example/entities/Articles.java
@@ -4,7 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -28,5 +28,5 @@ public class Articles {
   private String url;
   private String explanation;
   private String email;
-  LocalDateTime dateAdded;
+  private ZonedDateTime dateAdded;
 }
